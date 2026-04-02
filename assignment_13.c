aa2df58fb18aa75c1ac0673dd66552c4e92701fa
akshat-301 <gopalagarwal8899@gmail.com>
2026-04-02 05:33:44 +0000

    assignment_13

---
+++ assignment_13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Digits in reverse order: ");
+    while (num > 0) {
+        digit = num % 10;   // extract last digit
+        printf("%d", digit);
+        num = num / 10;     // remove last digit
+    }
+    printf("\n");
+
+    return 0;
+}
